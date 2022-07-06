6e87e92e6d91ff03e5be54eb1b21d917d7a81a80
mohammad ali nami <ali.nami1@yahoo.com>
2022-07-06 16:51:34 +0430

    full phase 1&2

--- c1.c
+++ c1.c
@@ -8,7 +8,7 @@
 #include "dirent.h"
 #include  <conio.h>
 #include   <math.h>
-#include   <time.h>
+
 
 
 
@@ -18,18 +18,33 @@ int menu_num = 1;
 COORD coord;
 CONSOLE_CURSOR_INFO     cursorInfo;
 FILE* view;
+
 char dir[1000][1000];
+char get_username[100];
 int len_dir;
 
 typedef struct Commponent {
-	char name[100];
-	char label[4];
-	char radio[5];
-	int hight;
-	int weidth;
-	int x;
-	int y;
+	char name[100];//name of text_box
+	char label[4] ;// yes\0, no\0
+	char radio[4];//  yes\0, no\0
+	char user_label[150];
+	char user_txt[7500];
+	char user_radio = 'f';
+	int hight ;
+	int width ;
+	int x ;
+	int y ;
 }Commponent;
+
+typedef struct arr_commponent {
+	Commponent* arr;
+	int size;
+}arr_commponent;
+
+typedef struct user_data {
+	char name[100];
+	char string[10000];
+}user_data;
 
 #include "functions.h"
 #define maxsize 200
@@ -42,12 +57,10 @@ typedef struct Commponent {
 	}
  }
 	 void menu() {
-		cursorInfo.dwSize = 100;
-		cursorInfo.bVisible = 0;
-		//coord.X = 20;
-		coord.Y = 45;
-		SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
-		SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+		
+		position_cmd(0, 45)
+		show(0)
+		
 
 		switch (menu_num)
 		{
@@ -55,11 +68,10 @@ typedef struct Commponent {
 			make_border();
 			printf( BGblue);
 			printf("\n");
-			if (position == 0)printf(white"      make form          "); else printf(black"      make form          ");
-			if (position == 1)printf(white"        save and edit          "); else printf(black"        save and edit          ");
-			if (position == 2)printf(white"        run commands          "); else printf(black"        run commands          ");
-			if (position == 3)printf(white"          exit          "); else printf(black"          exit          ");
-			printf(white" to navigate->a for left ,d for right   ");
+			if (position == 0)printf(white"                       Component           "); else printf(black"                       Component           ");
+			if (position == 1)printf(white"                    run commands          "); else printf(black"                    run commands          ");
+			if (position == 2)printf(white"          exit          "); else printf(black"          exit          ");
+			printf(white"     to navigate->a for left ,d for right");
 			printf("\n%s\n%s",BGwhite, BGblack);
 			break;
 		}
@@ -67,11 +79,10 @@ typedef struct Commponent {
 			make_border();
 			printf(BGred);
 			printf("\n");
-			if (position == 0)printf(white"        text box          "); else printf(black"        text box          ");
-			if (position == 1)printf(white"        label          "); else printf(black"        label          ");
-			if (position == 2)printf(white"    Radio button          "); else printf(black"    Radio button          ");
-			if (position == 3)printf(white"          back         "); else printf(black"          back         ");
-			printf(white"      to navigate->a for left ,d for right          ");
+			if (position == 0)printf(white"           make Component           "); else printf(black"           make Component           ");
+			if (position == 1)printf(white"           edit Component           "); else printf(black"           edit Component           ");
+			if (position == 2)printf(white"            back         "); else printf(black"            back         ");
+			printf(white"                 to navigate->a for left ,d for right");
 			printf("\n%s\n%s", BGwhite, BGblack);
 			break;
 		}
@@ -90,10 +101,9 @@ typedef struct Commponent {
 			make_border();
 			printf(BGgreen);
 			printf("\n");
-			if (position == 0)printf(white"        text box          "); else printf(black"        text box          ");
-			if (position == 1)printf(white"        label          "); else printf(black"        label          ");
-			if (position == 2)printf(white"          button          "); else printf(black"          button          ");
-			if (position == 3)printf(white"          back         "); else printf(black"          back         ");
+			if (position == 0)printf(white"        make text box          "); else printf(black"        text box          ");
+			if (position == 1)printf(white"        edit text box          "); else printf(black"        label          ");
+			if (position == 2)printf(white"          back         "); else printf(black"          back         ");
 			printf(white"      to navigate->a for left ,d for right          ");
 			printf("\n%s\n%s", BGwhite, BGblack);
 			break;
@@ -112,7 +122,7 @@ int on_press() {
 	menu();
 	while (keyboard_press != 13) {
 		keyboard_press = _getch();
-		if (keyboard_press == 100 && ((menu_num == 1 && position < 3) || (menu_num == 2 && position < 3) || (menu_num == 3 && position < 2) || (menu_num == 4 && position < 5))) {
+		if (keyboard_press == 100 && ((menu_num == 1 && position < 2) || (menu_num == 2 && position < 2) || (menu_num == 3 && position < 2) || (menu_num == 4 && position < 5))) {
 			position++;
 			menu();
 		}
@@ -125,21 +135,24 @@ int on_press() {
 	switch (menu_num)
 	{
 		case(1): {
-			if (position == 3) { exit(1); }
+			if (position == 2) { exit(1); }
 			menu_num = position + 2;
 			position = 0;
 			return 0;
 		}
 		case(2): {
 			
-			if (position == 3) { menu_num = 1; position = 0; return 0; }
+			if (position == 2) { menu_num = 1; position = 0; return 0; }
 		
-			if (position == 1)
+			if (position == 0)
 			{ 
 				system("cls");
-				//txt_box();
+				txt_box(0);
 			}
-			//menu_num = position + 2;
+			if (position == 1) {
+				system("cls");
+				txt_box(1);
+			}
 			position = 0;
 			return 0;
 		}
@@ -156,19 +169,46 @@ int on_press() {
 	}
 ////////////////////////////////////////////////////////////////////////////////////////////////
 void view_(FILE* file) {
-	char ch;
+	/*char ch;
 	rewind(file);
 	system("cls");
-	cursorInfo.dwSize = 100;
-	cursorInfo.bVisible = 0;
-	coord.Y = 0;
-	coord.X = 0;
-	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-	do {
-		ch = fgetc(file);
+	show(0)
+	position_cmd(0, 0)
+	ch = fgetc(file);
+	while (ch != EOF) {
 		printf(white"%c", ch);
-	} while (ch != EOF);
+		ch = fgetc(file);
+	} */
+	char info_dir[100] = "./info/";
+	strcat(info_dir, get_username);
+	file = fopen(info_dir, "r+");
+	arr_commponent* array = read_info(file);
+	
+	for (size_t i = 0; i < array->size; i++)
+	{
+		int k = 0;
+		position_cmd((array->arr + i)->x - 1, (array->arr + i)->y);
+		if (((array->arr + i)->radio)[0] == 'y' && (array->arr+i)->user_radio == 't') {printf(green"O");}
+		else if (((array->arr + i)->radio)[0] == 'y'){ printf(white"O"); }
+		position_cmd((array->arr + i)->x, (array->arr + i)->y);
+		while (k <= (array->arr + i)->width + 1) { printf(white"-"); k++; }
+		k = 1;
+		while (k <= (array->arr + i)->hight) {
+			position_cmd((((array->arr + i)->x + 1) + (array->arr + i)->width), ((array->arr + i)->y) + k)
+				printf(".");
+			k++;
+		}
+		k = 1;
+		while (k <= (array->arr + i)->hight)
+		{
+			position_cmd(((array->arr + i)->x), ((array->arr + i)->y) + k)
+				printf(".");
+			k++;
+		}
+		k = 0;
+		position_cmd(((array->arr + i)->x), ((array->arr + i)->y) + (array->arr + i)->hight + 1)
+			while (k <= (array->arr + i)->width + 1) { printf("-"); k++; }
+	}
 }
 ////////////////////////////////////////////////////////////////////////////////////////////////
 int navigation() {
@@ -185,6 +225,8 @@ int navigation() {
 ////////////////////////////////////////////////////////////////////////////////////////////////
 int main()
 {
+	
+	show(0)
 	len_dir = readdir_(dir);
 	system("mode con:cols=150 lines=50");
 	printf(green R"EOF(
--- functions.h
+++ functions.h
@@ -1,4 +1,9 @@
-
+#define show(x) cursorInfo.bVisible = x;\
+cursorInfo.dwSize = 100;\
+SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
+#define position_cmd(x,y) coord.X = x;\
+coord.Y = y;\
+SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 void make_border() {
 	for (int i = 0; i < 15; i++)
 	{
@@ -29,11 +34,25 @@ void clearBuffer() {
     } while (c != '\n' && c != EOF);
 }
 
+void write_(arr_commponent Commponents, FILE* file) {
+	rewind(file);
+	fwrite(&Commponents.size, sizeof(int), 1, file);
+	for (int i = 0 ; i< Commponents.size;i++)
+	{
+		fwrite(Commponents.arr + i, sizeof(Commponent), 1, file);
+	}
+	fclose(file);
+}
+
 FILE* sign_in()
 {
 	FILE* file = NULL;
+	FILE* file_info;
 	char username[100], info[105]="./info/";
 	int scan;
+	arr_commponent* array_ = (arr_commponent*)malloc(sizeof(arr_commponent));
+	array_->arr = (Commponent*)malloc(100 * sizeof(Commponent));
+	show(1)
 	while (1) {
 		system("cls");
 		printf("please enter your user name (only lowercase and number are allowed)\n");
@@ -56,7 +75,88 @@ FILE* sign_in()
 	{
 		file = fopen(username, "w+");
 		strcat(info, username);
-		fopen(info, "w");
+		file_info = fopen(info, "w");
+		array_->arr = NULL;
+		array_->size = 0;
+		fwrite(&array_->size, sizeof(arr_commponent), 1, file_info);
+		fclose(file_info);
 	}
+	
+	strcpy(get_username, username);
+	show(0)
 	return file;
+}
+
+arr_commponent* read_info(FILE* file) {
+	
+	arr_commponent* array = (arr_commponent*)malloc(sizeof(arr_commponent));
+	array->arr = (Commponent*)malloc(100 * sizeof(Commponent));
+	int i = 0;
+	
+	fread(&array->size, sizeof(int), 1, file);
+	while (i != array->size)
+	{
+		fread(&array->arr[i], sizeof(Commponent), 1, file);
+		i++;
+	}
+	return array;
+};
+
+
+
+
+void txt_box(int n) {
+	
+	Commponent text_box;
+	char arr[6600];
+	FILE* user_view;
+	FILE* file;
+	char info_dir[100] = "./info/";
+	strcat(info_dir, get_username);
+	file = fopen(info_dir, "r+");
+	arr_commponent* array = read_info(file);
+	printf("\t");
+	printf(red R"EOF(      make your text box with this pattern(screen is X = 150 * 50 = Y character so make your sure your txt box is inside screen)
+		                     "name label radio_box X Y hight width" e.g.: textbox1 yes no 10 10 10 10)EOF");
+	if (array->size == 0) {printf("\nno text box available\n");
+	}else {
+		printf(cyan"\nthere are %d text box available\n", array->size);
+		for (int j = 0; j < array->size; j++)
+		{
+			printf("name=%s \tlabel=%s \tradio_Box=%s \tX=%d \tY=%d \thight=%d \twidth=%d\n", (array->arr + j)->name ,
+				(array->arr + j)->label, (array->arr + j)->radio,
+				(array->arr + j)->x, (array->arr + j)->y,
+				(array->arr + j)->hight, (array->arr + j)->width);
+		}
+	}
+	show(1)
+		if (n == 0) {
+			printf(white"\nmake your text box\n");
+			scanf("%s %s %s %d %d %d %d", text_box.name,
+				text_box.label, text_box.radio, &text_box.x,
+				&text_box.y, &text_box.hight, &text_box.width);
+			
+			*(array->arr + array->size) = text_box;
+			show(0)
+			clearBuffer();
+			array->size++;
+			write_(*array, file);
+
+		}else{
+			char temp1[100];
+			printf("text box name: ");
+			scanf("%s", temp1);
+			printf("i.e. no no 10 10 10 10: ");
+			scanf("%s %s %d %d %d %d",
+				text_box.label, text_box.radio, &text_box.x,
+				&text_box.y, &text_box.hight, &text_box.width);
+			for (int i = 0; i < array->size; i++)
+			{
+				if( strcmp((array->arr+i)->name,temp1) == 0){
+					*(array->arr + i) = text_box;
+					write_(*array, file); break;
+				}
+			}
+		}
+	system("cls");
 }
